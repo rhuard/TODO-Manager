98bc679799f7894255d2b7be07f95b69e2760add
rhuard <rhuard13@gmail.com>
2016-08-08 21:24:59 -0700

    import errors

--- todom/backendbind.py
+++ todom/backendbind.py
@@ -1,8 +1,8 @@
 #contains the backend bindings for the UIs to import and use
 
-from item.items import Item
-from priority.priority import *
-from tags.tag import Tag
+from todom.item.items import Item
+from todom.priority.priority import *
+from todom.tags.tag import Tag
 
 class Backend():
 
