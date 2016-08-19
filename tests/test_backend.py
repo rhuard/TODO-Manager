8367fd8ecd0f289ffd238ece7f311d068ed852fb
Ryan Huard <rhuard13@gmail.com>
2016-08-19 14:45:12 -0700

    Update test_backend.py

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,5 +1,5 @@
-from TODO-Manager.todom.backendbind import Backend as Back
-from TODO-Manager.todom.priority.priority import *
+from TODOManager.todom.backendbind import Backend as Back
+from TODOManager.todom.priority.priority import *
 
 def test_insert():
     b = Back()
